a9382fe8a7583c3255800bdeaae191f1c6659dcf
ekoi <eko.indarto@dans.knaw.nl>
2024-03-21 13:41:08 +0100

    Modified based on Kerim's suggestion

--- registry/frontend/src/components/Reviews.tsx
+++ registry/frontend/src/components/Reviews.tsx
@@ -1,15 +1,43 @@
 import {Vocab} from '../misc/interfaces';
-import ReviewForm from "./ReviewForm";
-import StarRating from "./StarRating";
 import useAuth from "../hooks/useAuth";
 import ReportAbuse from "./ReportAbuse";
 import React from "react";
 import {useNavigate} from "react-router-dom";
 
+import {StarRating} from "review-component-react"
+import ReviewForm from "./ReviewForm"
+
+
 
 export default function Reviews({data}: { data: Vocab }) {
     const [userInfo] = useAuth();
     const navigate = useNavigate();
+    const url_target = `/review/${data.id}`
+
+    const handleRating = async (stars, text, setSuccess) => {
+
+               await fetch(url_target, {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                        'Access-Control-Allow-Origin': '*'
+                    },
+                    body: JSON.stringify({
+                        "user": "user",
+                        "rating": Number(stars),
+                        "review": text
+
+                    })
+
+                }).then(response => {
+                        console.log("********" + response.status)
+                        console.log(response)
+                        // submit was succesful
+                        setSuccess(true);
+                 }
+                );
+    }
 
     const login = () => {
        window.location.href = '/login'
@@ -19,9 +47,7 @@ export default function Reviews({data}: { data: Vocab }) {
             {data.reviews.map(review => (
                 <div className="review" key={review.id}>
                     <div className="hcAlignLeft hcMarginBottom1">
-                        <div>
                             <StarRating page_id={review.id} rate={review.rating} user_id={review.id} readonly={true} handleRating={0}/>
-                        </div>
                         {review.nickname && <span>{review.nickname}</span>}
                     </div>
 
@@ -30,7 +56,7 @@ export default function Reviews({data}: { data: Vocab }) {
             ))}
             {!userInfo ?
             <button onClick={login} >Do you want write a review? Please log in.</button> :
-            <div><ReviewForm id={data.id} user={data.user} /></div>
+            <div><ReviewForm id={data.id} user={data.user} handleRating={handleRating}/></div>
         }
 
             <div><ReportAbuse/></div>
