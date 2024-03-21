ed49d7eab3af8f30daa559d01c72d880faaf9f02
ekoi <eko.indarto@dans.knaw.nl>
2024-03-21 13:34:03 +0100

    Revert to previous version

--- registry/frontend/src/components/Facets.tsx
+++ registry/frontend/src/components/Facets.tsx
@@ -1,16 +1,8 @@
 import React from 'react';
 import {FreeTextFacet, ListFacet, ISendCandidate} from 'browser-base-react';
-import useAuth from "../hooks/useAuth";
 
 export default function Facets({sendCandidateHandler}: { sendCandidateHandler: ISendCandidate }) {
-    const [userInfo] = useAuth();
     return <>
-
-        {!userInfo ?
-            <button >Log in</button> :
-            <button >welcome ${userInfo.family_name}</button>
-        }
-
         <FreeTextFacet add={sendCandidateHandler}/>
         <ListFacet parentCallback={sendCandidateHandler}
                    name="Type of vocabulary"
